9b9928c961ec1335134e4b25fa4c589fc62dc42c
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-21 20:42:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ import { MenuScene } from './src/scenes/MenuScene.js';
 
 const config = {
   type: Phaser.AUTO,
+
   width: 360,
   height: 640,
+
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+
   backgroundColor: '#000',
   scene: [BootScene, MenuScene]
 };
