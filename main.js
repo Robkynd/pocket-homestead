730429343edfc01761f1a548d0d2209d7e1731d3
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-21 21:06:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-import { BootScene } from './src/scenes/BootScene.js';
-import { MenuScene } from './src/scenes/MenuScene.js';
+import BootScene from './scenes/BootScene.js';
+import MenuScene from './scenes/MenuScene.js';
+import FarmScene from './scenes/FarmScene.js';
 
 const config = {
   type: Phaser.AUTO,
-
-  width: 360,
-  height: 640,
-
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: '#000000',
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
-
-  backgroundColor: '#000',
-  scene: [BootScene, MenuScene]
+  scene: [BootScene, MenuScene, FarmScene]
 };
 
 new Phaser.Game(config);
