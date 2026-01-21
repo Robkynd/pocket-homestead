7ff98f4d1141cc57937d67e2c7bc9183b082ff51
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-21 21:41:24 +0700

    Update FarmScene.js

--- src/scenes/FarmScene.js
+++ src/scenes/FarmScene.js
@@ -3,32 +3,13 @@ export default class FarmScene extends Phaser.Scene {
     super('FarmScene');
   }
 
-  preload() {
-    this.load.image('grass', 'assets/tiles/grass.png');
-    this.load.image('farmer', 'assets/player/farmer.png');
-  }
-
   create() {
-    // background ladang
-    this.add.tileSprite(
-      0,
-      0,
-      this.scale.width,
-      this.scale.height,
-      'grass'
-    ).setOrigin(0);
+    console.log('FarmScene loaded');
 
-    // player
-    this.player = this.add.image(
-      this.scale.width / 2,
-      this.scale.height / 2,
-      'farmer'
-    ).setScale(2);
+    // background hijau ladang
+    this.add.rectangle(0, 0, this.scale.width, this.scale.height, 0x00ff00).setOrigin(0);
 
     // teks debug
-    this.add.text(10, 10, 'Farm Scene', {
-      fontSize: '16px',
-      color: '#ffffff'
-    });
+    this.add.text(20, 20, 'FARM SCENE OK', { color: '#000' });
   }
 }
