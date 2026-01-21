b57659034e67404a43a76574523187eb83f39f63
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-21 20:04:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-import { gameConfig } from './src/config/gameConfig.js';
+window.onload = () => {
+  const config = {
+    type: Phaser.AUTO,
+    width: 360,
+    height: 640,
+    backgroundColor: '#87CEEB',
+    scene: {
+      create() {
+        this.add.text(50, 50, 'Pocket Homestead OK', {
+          fontSize: '20px',
+          color: '#000'
+        });
+      }
+    }
+  };
 
-new Phaser.Game(gameConfig);
+  new Phaser.Game(config);
+};
