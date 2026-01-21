a0d558731c38959e2a28ec4c0d9f0c49b1ca757d
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-22 00:38:08 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-import { BootScene } from './src/scenes/BootScene.js';
-import { MenuScene } from './src/scenes/MenuScene.js';
-import { FarmScene } from './src/scenes/FarmScene.js';
+import BootScene from './src/scenes/BootScene.js';
+import MenuScene from './src/scenes/MenuScene.js';
+import FarmScene from './src/scenes/FarmScene.js';
 
 const config = {
   type: Phaser.AUTO,
-
   width: 360,
   height: 640,
-
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   },
-
   backgroundColor: '#000',
   scene: [BootScene, MenuScene, FarmScene]
 };
