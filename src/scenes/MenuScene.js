a18d59a0931e396eaf22a5756c35f987e81afc96
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-21 21:40:49 +0700

    Update MenuScene.js

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -6,14 +6,23 @@ export default class MenuScene extends Phaser.Scene {
   create() {
     console.log('MenuScene loaded');
 
-    // background
+    // background biru muda
     this.add.rectangle(0, 0, this.scale.width, this.scale.height, 0x87ceeb).setOrigin(0);
 
-    // title
-    this.add.text(this.scale.width / 2, 120, 'POCKET HOMESTEAD', { fontSize: '24px', color: '#000' }).setOrigin(0.5);
+    // title game
+    this.add.text(this.scale.width / 2, 120, 'POCKET HOMESTEAD', {
+      fontSize: '24px',
+      color: '#000',
+      fontStyle: 'bold'
+    }).setOrigin(0.5);
 
-    // start
-    const startText = this.add.text(this.scale.width / 2, 220, 'TAP TO START', { fontSize: '18px', color: '#000' }).setOrigin(0.5);
+    // tombol start
+    const startText = this.add.text(this.scale.width / 2, 220, 'TAP TO START', {
+      fontSize: '18px',
+      color: '#000'
+    }).setOrigin(0.5);
+
+    // buat bisa di tap
     startText.setInteractive({ useHandCursor: true });
 
     startText.on('pointerdown', () => {
