f85d712d8068eab48c41e3a8c1048943a8fe2178
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-21 21:35:41 +0700

    Update MenuScene.js

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -4,43 +4,20 @@ export default class MenuScene extends Phaser.Scene {
   }
 
   create() {
-    // background (biar ga putih)
-    this.add.rectangle(
-      0,
-      0,
-      this.scale.width,
-      this.scale.height,
-      0x87ceeb // biru muda
-    ).setOrigin(0);
+    console.log('MenuScene loaded');
+
+    // background
+    this.add.rectangle(0, 0, this.scale.width, this.scale.height, 0x87ceeb).setOrigin(0);
 
     // title
-    this.add.text(
-      this.scale.width / 2,
-      120,
-      'POCKET HOMESTEAD',
-      {
-        fontSize: '24px',
-        color: '#000',
-        fontStyle: 'bold'
-      }
-    ).setOrigin(0.5);
+    this.add.text(this.scale.width / 2, 120, 'POCKET HOMESTEAD', { fontSize: '24px', color: '#000' }).setOrigin(0.5);
 
-    // tombol start
-    const startText = this.add.text(
-      this.scale.width / 2,
-      220,
-      'TAP TO START',
-      {
-        fontSize: '18px',
-        color: '#000'
-      }
-    ).setOrigin(0.5);
-
-    // WAJIB
+    // start
+    const startText = this.add.text(this.scale.width / 2, 220, 'TAP TO START', { fontSize: '18px', color: '#000' }).setOrigin(0.5);
     startText.setInteractive({ useHandCursor: true });
 
     startText.on('pointerdown', () => {
-      console.log('Start tapped');
+      console.log('TAP TO START clicked');
       this.scene.start('FarmScene');
     });
   }
