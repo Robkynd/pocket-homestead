7cb00713abb6a9d3e174bc5726e36f30dc476332
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-21 21:15:43 +0700

    Update MenuScene.js

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -1,38 +1,47 @@
-export class MenuScene extends Phaser.Scene {
+export default class MenuScene extends Phaser.Scene {
   constructor() {
     super('MenuScene');
   }
 
   create() {
-    const { width, height } = this.scale;
+    // background (biar ga putih)
+    this.add.rectangle(
+      0,
+      0,
+      this.scale.width,
+      this.scale.height,
+      0x87ceeb // biru muda
+    ).setOrigin(0);
 
-    // BACKGROUND IMAGE
-    this.add.image(width / 2, height / 2, 'title')
-      .setDisplaySize(width, height);
-
-    // BUTTON LOGIN
-    const btn = this.add.text(
-      width / 2,
-      height - 90,
-      'LOG IN',
+    // title
+    this.add.text(
+      this.scale.width / 2,
+      120,
+      'POCKET HOMESTEAD',
       {
-        fontSize: '22px',
-        color: '#fff',
-        backgroundColor: '#6b4f2c',
-        padding: { x: 18, y: 10 }
+        fontSize: '24px',
+        color: '#000',
+        fontStyle: 'bold'
       }
-    )
-    .setOrigin(0.5)
-    .setInteractive();
+    ).setOrigin(0.5);
 
-    btn.on('pointerdown', () => {
-      alert('MASUK GAME');
-      // this.scene.start('FarmScene');
-      
-    });startText.setInteractive();
+    // tombol start
+    const startText = this.add.text(
+      this.scale.width / 2,
+      220,
+      'TAP TO START',
+      {
+        fontSize: '18px',
+        color: '#000'
+      }
+    ).setOrigin(0.5);
 
-startText.on('pointerdown', () => {
-  this.scene.start('FarmScene');
-});
+    // WAJIB
+    startText.setInteractive({ useHandCursor: true });
+
+    startText.on('pointerdown', () => {
+      console.log('Start tapped');
+      this.scene.start('FarmScene');
+    });
   }
 }
