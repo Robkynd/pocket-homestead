628be030591d1e14effd2630adb804371009d11f
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-21 20:18:09 +0700

    Update MenuScene.js

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -4,28 +4,13 @@ export class MenuScene extends Phaser.Scene {
   }
 
   create() {
-    const { width, height } = this.scale;
+    this.add.text(40, 200, 'MENU OK - TAP TO START', {
+      fontSize: '18px',
+      color: '#000'
+    });
 
-    // background image
-    this.add.image(width / 2, height / 2, 'title')
-      .setDisplaySize(width, height);
-
-    // LOGIN BUTTON
-    const loginBtn = this.add.text(
-      width / 2,
-      height - 100,
-      'LOG IN',
-      {
-        fontSize: '24px',
-        color: '#ffffff',
-        backgroundColor: '#6b4f2c',
-        padding: { x: 20, y: 10 }
-      }
-    ).setOrigin(0.5)
-     .setInteractive();
-
-    loginBtn.on('pointerdown', () => {
-      this.scene.start('FarmScene');
+    this.input.once('pointerdown', () => {
+      console.log('tap works');
     });
   }
 }
