c56681cd3207e3e6069f939a9d8d3012d626beea
Robkynd <81851344+Robkynd@users.noreply.github.com>
2026-01-22 00:14:01 +0700

    Update FarmScene.js

--- src/scenes/FarmScene.js
+++ src/scenes/FarmScene.js
@@ -7,14 +7,8 @@ export default class FarmScene extends Phaser.Scene {
     // Load tiles
     this.load.image('grass', 'assets/tiles/grass.png');
     this.load.image('chickenCoop', 'assets/objects/chickencoop.png');
-    this.load.image('cowBarn', 'assets/objects/barn.png');
-   
+    this.load.image('barn', 'assets/objects/barn.png');  
   }
-
-  create() {
-    const tileSize = 64;
-    const mapWidth = 8;
-    const mapHeight = 10;
 
     // --- UI BAR ---
     this.add.rectangle(0, 0, this.scale.width, tileSize, 0xcccccc).setOrigin(0);
@@ -27,32 +21,7 @@ export default class FarmScene extends Phaser.Scene {
       }
     }
 
-    // --- Mini farms 3x3 ---
-    const miniFarms = [
-      { startX: 1, startY: 2 }, // farm 1
-      { startX: 4, startY: 2 }  // farm 2
-    ];
-
-    miniFarms.forEach(farm => {
-      for (let y = 0; y < 3; y++) {
-        for (let x = 0; x < 3; x++) {
-          const soilTile = this.add.image((farm.startX + x)*tileSize, (farm.startY + y)*tileSize + tileSize, 'soil').setOrigin(0);
-          soilTile.setInteractive();
-          soilTile.on('pointerdown', () => {
-            console.log(`Mini farm tapped: x=${farm.startX + x}, y=${farm.startY + y}`);
-          });
-        }
-      }
-    });
-
-    // --- Fence around map ---
-    for (let x = 0; x < mapWidth; x++) {
-      this.add.image(x*tileSize, tileSize + 0*tileSize, 'fence').setOrigin(0); // top
-      this.add.image(x*tileSize, tileSize + (mapHeight-1)*tileSize, 'fence').setOrigin(0); // bottom
-    }
-    for (let y = 0; y < mapHeight; y++) {
-      this.add.image(0, tileSize + y*tileSize, 'fence').setOrigin(0); // left
-      this.add.image((mapWidth-1)*tileSize, tileSize + y*tileSize, 'fence').setOrigin(0); // right
+ 
     }
 
     // --- Barns / coop ---
